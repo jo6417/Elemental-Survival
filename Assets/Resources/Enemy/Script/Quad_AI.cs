dbf6040b6646e376d255cbb5b7bb63b6be3a81ec
jo6417 <jo6417@gmail.com>
2022-08-06 01:18:46 +0900

    Quad 보스 디자인 수정, 톱날 투사체 구현
    1. Quad 보스 디자인 수정
     - 모든 부위 사이즈 키워서 수정
     - 날개 스프라이트 선풍기 날 모양으로 디자인 수정
    2. 보스가 사용할 톱날 투사체 구현

--- Assets/Resources/Enemy/Script/Quad_AI.cs
+++ Assets/Resources/Enemy/Script/Quad_AI.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Lean.Pool;
 using TMPro;
 using UnityEngine;
 
@@ -8,10 +9,14 @@ public class Quad_AI : MonoBehaviour
     [Header("State")]
     [SerializeField]
     Patten patten = Patten.None;
-    enum Patten { PushSide, PushCircle, BladeShot, FanSmash, None };
+    enum Patten { PushSide, PushCircle, BladeShot, FanSwing, None };
     private float coolCount;
+    public float pushSideCoolTime;
+    public float pushCircleCoolTime;
+    public float bladeShotCoolTime;
+    public float fanSwingCoolTime;
     public float atkRange = 30f; // 공격 범위
-    Vector3 playerPos; // 플레이어 추정 위치
+    Vector3 targetPos; // 추적할 위치
     public float fanSensitive = 0.1f; //프로펠러 기울기 감도 조절
 
     [Header("Refer")]
@@ -20,6 +25,11 @@ public class Quad_AI : MonoBehaviour
     public Transform body;
     public Transform eye;
     public Transform[] fans = new Transform[4]; // 프로펠러들
+
+    [Header("PushSide")]
+    public GameObject wallBox; // 플레이어 가두기 위한 상자 콜라이더
+    public GameObject sawBladeHorizon; // 가로 톱날
+    public GameObject sawBladeVertical; // 세로 톱날
 
     private void OnEnable()
     {
@@ -71,10 +81,10 @@ public class Quad_AI : MonoBehaviour
             return;
 
         // 플레이어 추정 위치 계산
-        playerPos = PlayerManager.Instance.transform.position + (Vector3)Random.insideUnitCircle * 3f;
+        targetPos = PlayerManager.Instance.transform.position + (Vector3)Random.insideUnitCircle * 3f;
 
         // 플레이어 방향
-        Vector2 dir = playerPos - transform.position;
+        Vector2 dir = targetPos - transform.position;
 
         // 플레이어와의 거리
         float distance = dir.magnitude;
@@ -88,16 +98,16 @@ public class Quad_AI : MonoBehaviour
         // 쿨타임 됬을때, 범위 내에 있을때
         if (distance <= atkRange)
         {
-            // if (coolCount <= 0)
-            // {
-            //     // 속도 초기화
-            //     enemyManager.rigid.velocity = Vector3.zero;
+            if (coolCount <= 0)
+            {
+                // 속도 초기화
+                enemyManager.rigid.velocity = Vector3.zero;
 
-            //     //공격 패턴 결정하기
-            //     ChooseAttack();
+                //공격 패턴 결정하기
+                ChooseAttack();
 
-            //     return;
-            // }
+                return;
+            }
         }
         else
             // 플레이어 따라가기
@@ -118,15 +128,15 @@ public class Quad_AI : MonoBehaviour
         if (patten != Patten.None)
             randomNum = (int)patten;
 
-        // switch (randomNum)
-        // {
-        //     case 0:
-        //         // 주먹 내려찍기 패턴
-        //         StartCoroutine(FistDrop());
-        //         //쿨타임 갱신
-        //         coolCount = fistDropCoolTime;
-        //         break;
-        // }
+        switch (randomNum)
+        {
+            case 0:
+                // 주먹 내려찍기 패턴
+                StartCoroutine(PushSide());
+                //쿨타임 갱신
+                coolCount = pushSideCoolTime;
+                break;
+        }
     }
 
     void Walk()
@@ -168,5 +178,48 @@ public class Quad_AI : MonoBehaviour
         enemyManager.rigid.velocity = playerDir.normalized * enemyManager.speed * SystemManager.Instance.globalTimeScale;
 
         enemyManager.nowAction = EnemyManager.Action.Idle;
+    }
+
+    IEnumerator PushSide()
+    {
+        //todo 플레이어 위치에 벽 오브젝트 소환해서 플레이어 가두기
+        GameObject wall = LeanPool.Spawn(wallBox, PlayerManager.Instance.transform.position, Quaternion.identity);
+        SpriteRenderer wallSprite = wall.GetComponent<SpriteRenderer>();
+
+        //todo 벽 모서리 위치 계산
+        Vector2 leftUpEdge = new Vector2(
+            wall.transform.position.x - wallSprite.bounds.size.x / 2f,
+            wall.transform.position.y + wallSprite.bounds.size.y / 2f);
+        Vector2 rightUpEdge = new Vector2(
+            wall.transform.position.x + wallSprite.bounds.size.x / 2f,
+            wall.transform.position.y + wallSprite.bounds.size.y / 2f);
+        Vector2 leftDownEdge = new Vector2(
+            wall.transform.position.x - wallSprite.bounds.size.x / 2f,
+            wall.transform.position.y - wallSprite.bounds.size.y / 2f);
+        Vector2 rightDownEdge = new Vector2(
+            wall.transform.position.x + wallSprite.bounds.size.x / 2f,
+            wall.transform.position.y - wallSprite.bounds.size.y / 2f);
+
+        //todo 모서리 사이 거리 계산
+        //todo 톱니 사이즈로 거리 나눠서 톱니 위치 포인트 산출
+        //todo 해당 포인트에 톱니 소환
+        //todo 톱니 domove, Ease.InBack 으로 바닥에 박기
+
+        //todo 좌,우 중 한쪽 벽 가운데로 이동해 프로펠러를 일렬로 배치
+
+        //todo 보스는 대각으로 몸체 기울이며 바람쏴서 플레이어를 밀어냄
+        //todo 플레이어 속도에 비례한 힘으로 밀어내기, 보스쪽으로 이동 누르면 조금씩 나아갈정도
+        //todo 페이즈가 오르면 해당 패턴에서 플레이어쪽 X방향으로 윈드커터 랜덤하게 발사
+
+        // 벽 비활성화
+        // wall.SetActive(false);
+
+        //todo 과부하로 착지해서 휴식
+        //todo 연기 파티클, 불꽃 파티클
+
+        yield return null;
+
+        // Idle 상태로 전환
+        // enemyManager.nowAction = EnemyManager.Action.Idle;
     }
 }
